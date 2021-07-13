9a63a7ca4b6b53c39b52172ae1e7e06c4c4e5887
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2021-07-13 17:48:01 +0100

    made child component class component

--- src/LazyLoad.vue
+++ src/LazyLoad.vue
@@ -3,3 +3,12 @@
     This is a test component to be loaded lazily with import('...')
   </p>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class LazyLoad extends Vue {}
+</script>
